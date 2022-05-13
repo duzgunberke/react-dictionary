9cb56ff7da58ef6f7bfc3504fbea0a62deda98d5
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-05-13 15:00:10 +0300

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+        <a href="https://github.com/duzgunberke" target="__blank">
+          DZGN
+        </a>
+      </span>
+      <div className="iconContainer">
+        <a href="https://www.instagram.com/berke.dzgn/?hl=tr" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/berkeduzgun/" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
